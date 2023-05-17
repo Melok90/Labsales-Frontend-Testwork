c0746eac79a7af3ec9a04409d5facb90c831bd32
Aleksandr Leontev <al28059@mail.ru>
2023-05-17 22:32:12 +0200

    add pIp

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
-
-
+/////swiper////
 var swiper = new Swiper(".swiper-cost", {
     slidesPerView: 1,
     spaceBetween: 10,
-    
+
     loop: true,
     freeMode: true,
     autoplay: {
-        delay: 3000,
+        delay: 9000,
         disableOnInteraction: false,
     },
     navigation: {
@@ -18,7 +17,7 @@ var swiper = new Swiper(".swiper-cost", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-        
+
     },
     breakpoints: {
         768: {
@@ -37,11 +36,10 @@ var swiper = new Swiper(".swiper-cost", {
 });
 
 ////two swiper////
-
 var swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 10,
-    
+
     loop: true,
     freeMode: true,
     autoplay: {
@@ -55,7 +53,7 @@ var swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-        
+
     },
     breakpoints: {
         768: {
@@ -73,4 +71,73 @@ var swiper = new Swiper(".swiper", {
     },
 });
 
+const map_marker = document.getElementById('map-click');
+const active_marker = document.getElementById('geo-on-icon');
+const active_marker_2 = document.getElementById('geo-on-icon_2');
+const passive_marker = document.getElementById('geo-off-icon');
+const passive_marker_2 = document.getElementById('geo-off-icon_2');
+const text_active = document.getElementById('text_active');
 
+map_marker.onclick = function () {
+    if (active_marker.style.display === "none") {
+      active_marker.style.display = "block";
+      passive_marker.style.display = "none";
+      text_active.style.color = "#31bef4";
+    } else if (active_marker.style.display === "block") {
+      active_marker.style.display = "none";
+      passive_marker.style.display = "block";
+      text_active.style.color = "";
+    }
+  
+    if (active_marker_2.style.display === "none") {
+      active_marker_2.style.display = "block";
+      passive_marker_2.style.display = "none";
+    } else if (active_marker_2.style.display === "block") {
+      active_marker_2.style.display = "none";
+      passive_marker_2.style.display = "block";
+    }
+  };
+
+////popup////
+
+// const popUp_btn = document.getElementById('open-popUp');
+
+// popUp_btn.onclick = function()  {
+//     console.log('hello')
+// }
+
+const menu_button = document.getElementById('open-popUp');
+const popup_menu = document.getElementById('popUp_wrapper');
+const menu_close_btn = document.getElementById('close-popUp');
+
+menu_button.onclick = function() {
+    if (popup_menu.style.display === "none") {
+        popup_menu.style.display = "block";
+    } else {
+        popup_menu.style.display = "none";
+    }
+};
+
+menu_close_btn.onclick = function() {
+    popup_menu.style.display = "none";
+};
+
+
+
+
+
+
+
+
+//////////////////
+// window.addEventListener('resize', centerElement);
+
+// function centerElement() {
+//   var element = document.getElementById('myElement');
+//   element.style.top = '50%';
+//   element.style.left = '50%';
+//   element.style.transform = 'translate(-50%, -50%)';
+// }
+
+// centerElement();
+///////////////////
